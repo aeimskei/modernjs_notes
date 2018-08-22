9569d8c49adace98c365388b06172f550a257bd2
Amy Boudsady <aeimskei@gmail.com>
2018-08-22 16:24:35 -0700

    Initial commit with some console commands

---
+++ fundamentals/app.js
@@ -0,0 +1,23 @@
+// =========================================
+// Log to console
+// =========================================
+console.log('Hello World'); // string
+console.log(123); // number
+console.log(true); // boolean
+var greeting = "Hello World";
+console.log(greeting); // variable
+console.log([1,2,3,4]); // array
+console.log({a:1, b:2}); // object
+console.table({a:1, b:2}); // table
+
+console.error('This is some error'); // error
+console.clear(); // clears the console
+console.warn('This is a warning'); // warning
+console.time('Hello'); // timer begins
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello'); // timer ends
