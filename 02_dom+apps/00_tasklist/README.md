b2990ba65c2ed95b557e067c423c23e237c9e3a2
Amy Boudsady <aeimskei@gmail.com>
2018-08-29 13:14:05 -0700

    Task List App: Able to delete and filter tasks

--- 02_dom+apps/00_tasklist/README.md
+++ 02_dom+apps/00_tasklist/README.md
@@ -185,6 +185,153 @@ function addTask(e) {
 
 <kbd>![alt text](img/taskform.png "screenshot")</kbd>
 
+## Delete Task
+
+We need another event listener, each of the ```x``` icon has a class of ```delete-item``` and since there are mulitple of them and also dynamic, we need to use Event Delegation here. So that means, put the even listener on to the Task List itself, the ```<ul>```.
+
+Go into the ```init()``` function for the load event listeners and create that'll listen for a ```click``` and call ```removeTask``` we'll create that function later.
+
+```
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// INITIALIZE
+init();
+
+// FUNCTION TO LOAD EVENT LISTENERS
+function init() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+}
+
+// FUNCTION TO ADD TASK
+function addTask(e) {
+  // check if input field is empty
+  if (taskInput.value === '') {
+    alert('Please add a task');
+  }
+
+  // create new li for input value
+  const li = document.createElement('li');
+  li.className = 'collection-item'; // refering to Materialize's
+  li.appendChild(document.createTextNode(taskInput.value))
+
+  // create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content'; // refering to Materialize's
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+
+  // append li to ul
+  taskList.appendChild(li);
+
+  // clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+```
+
+**Remove Task Function**
+
+This function ```removeTask``` is going to take an event parameter ```e``` and then remember, we need to target the ```x``` icon. So, we need to check and create a condition for that particular target. Use ```e.target.parentElement.classList.contains``` to get the ```<a>``` link.
+
+Remember, we want the whole ```<li>``` to remove when the target element is clicked on. Right now the setup is, when you click on the target element, it logs ```<i>...</i>``` icon level, but we want to remove up to two levels of parent element, the ```<a>``` and then ```<li>```.
+
+```
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    // console.log(e.target);   // Test, logs <i>..</i>
+    e.target.parentElement.parentElement.remove();
+  }
+}
+```
+
+We can also add confirmation to check if the user really wants to remove the task.
+
+```
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    // console.log(e.target);   // Test, logs <i>..</i>
+    if (confirm('Are you sure you want to remove task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+```
+
+## Clear Task Button
+
+The while-loop, while there is a first child in the task list, ```taskList.firstChild``` meaning that there's still something in the list, then we want to ```taskList.removeChild(taskList.firstChild)```
+
+```
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // while loop is faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+```
+
+## Filter Through Tasks
+
+Add another event listener for filter, ```filter.addEventListener('keyup', filterTasks);``` inside the ```init()``` function, it's going to use the 'keyup' event type and call the filterTasks function.
+
+```
+// FUNCTION TO LOAD EVENT LISTENERS
+function init() {
+
+  // Add task event
+  form.addEventListener('submit', addTask);
+
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // Clear task event
+  clearButton.addEventListener('click', clearTasks);
+
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+```
+
+First, get the value of whatever that's being typed in the input field. Turn it to lowercase so we can match correctly even if the user accidently has the caps-lock on.
+
+Next, take all of the list items with ```document.querySelectorAll('.collection-item').forEach();``` and we can use ```forEach()``` bc ```querySelectorAll``` returns a ```NodeList```, but if we used ```getElementByClassName``` that would return an ```HTML Collection``` which we would have to convert to an array in order to use ```forEach()```. In the ```forEach()``` pass a function with ```task``` as a parameter for our iterator. Then, set a variable for ```item``` and set it to ```task.firstChild``` and then we want the ```textContent``` of that ```firstChild```. 
+
+Afterwards, create a condition to check inside of ```document.querySelectorAll('.collection-item').forEach(function(task) {...}``` if ```item.toLowerCase()``` to make in not case-sensitive, and ```indexOf(text)``` and pass in the ```text``` that's the text that's being typed, we want to search in our list and pass it to ```indexOf()```, if there's no match, it's going to equal to ```-1``` so we want to say, if it's not equal to negative one ```!=``` then we want the task to show, set it to show with ```task.style.display = 'block';``` else hide it with 
+```task.style.display = 'none';```
+
+```
+function filterTasks(e) {
+  // get the input field value
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+    // console.log(item);  // test
+  });
+
+  // console.log(text); // test, it'll log input on keyup
+}
+```
+
+<kbd>![alt text](img/filter.png "screenshot")</kbd>
+
+
 ## Technology
 * Vanilla JavaScript
 * HTML, Materialize CSS
--- 02_dom+apps/00_tasklist/tasklist.js
+++ 02_dom+apps/00_tasklist/tasklist.js
@@ -2,19 +2,19 @@
 // Define UI Variables
 // ======================================
 
-// get <form>
-const form = document.querySelector('#task-form');
-// get <ul>
-const taskList = document.querySelector('.collection');
-// get clear button
-const clearButton = document.querySelector('.clear-tasks');
-// get filter
-const filter = document.querySelector('#filter');
-// get input field
-const taskInput = document.querySelector('#task');
+// // get <form>
+// const form = document.querySelector('#task-form');
+// // get <ul>
+// const taskList = document.querySelector('.collection');
+// // get clear button
+// const clearButton = document.querySelector('.clear-tasks');
+// // get filter
+// const filter = document.querySelector('#filter');
+// // get input field
+// const taskInput = document.querySelector('#task');
 
 // ======================================
-// Function to Submit on Form
+// Add Task Function to Submit on Form
 // ======================================
 
 /*
@@ -23,14 +23,14 @@ Instead of putting it in the global scope, call a function to load all event lis
 Take the ```form``` variable we created and add an event listener with event type of submit as the first parameter, and for the second parameter, call the function ```addTask```, which we'll create as another function as our event handler to add new tasks.
 */
 
-// Load all event listeners
-init();
+// // Load all event listeners
+// init();
 
-// Function for load all event listeners
-function init() {
-  // Add task even
-  form.addEventListener('submit', addTask);
-}
+// // Function for load all event listeners
+// function init() {
+//   // Add task event
+//   form.addEventListener('submit', addTask);
+// }
 
 /*
 Our ```addTask``` function will take in one parameter of ```e``` to handle our event. Also add in ```e.preventDefault()``` to stop the default behavior which is a form submit.
@@ -106,6 +106,139 @@ Next, create a new link element for the ```x``` icon link to delete the task. Fi
 Finally, add li to ul, the <ul> is on the actual HTML we created earlier.
 */
 
+// function addTask(e) {
+//   // check if input field is empty
+//   if (taskInput.value === '') {
+//     alert('Please add a task');
+//   }
+
+//   // create new li for input value
+//   const li = document.createElement('li');
+//   li.className = 'collection-item'; // refering to Materialize's
+//   li.appendChild(document.createTextNode(taskInput.value))
+
+//   // create new link element
+//   const link = document.createElement('a');
+//   link.className = 'delete-item secondary-content'; // refering to Materialize's
+//   link.innerHTML = '<i class="fa fa-remove"></i>';
+//   li.appendChild(link);
+
+//   // append li to ul
+//   taskList.appendChild(li);
+
+//   // clear input
+//   taskInput.value = '';
+
+//   e.preventDefault();
+// }
+
+
+// ======================================
+// Delete Task Function
+// ======================================
+/*
+We need another event listener, each of the ```x``` icon has a class of ```delete-item``` and since there are mulitple of them and also dynamic, we need to use Event Delegation here. So that means, put the even listener on to the Task List itself, the ```<ul>```.
+
+Go into the ```init()``` function for the load event listeners and create that'll listen for a ```click``` and call ```removeTask``` we'll create that function later.
+*/
+
+// const form = document.querySelector('#task-form');
+// const taskList = document.querySelector('.collection');
+// const clearButton = document.querySelector('.clear-tasks');
+// const filter = document.querySelector('#filter');
+// const taskInput = document.querySelector('#task');
+
+// // INITIALIZE
+// init();
+
+// // FUNCTION TO LOAD EVENT LISTENERS
+// function init() {
+//   // Add task event
+//   form.addEventListener('submit', addTask);
+//   // Remove task event
+//   taskList.addEventListener('click', removeTask);
+// }
+
+// // FUNCTION TO ADD TASK
+// function addTask(e) {
+//   // check if input field is empty
+//   if (taskInput.value === '') {
+//     alert('Please add a task');
+//   }
+
+//   // create new li for input value
+//   const li = document.createElement('li');
+//   li.className = 'collection-item'; // refering to Materialize's
+//   li.appendChild(document.createTextNode(taskInput.value))
+
+//   // create new link element
+//   const link = document.createElement('a');
+//   link.className = 'delete-item secondary-content'; // refering to Materialize's
+//   link.innerHTML = '<i class="fa fa-remove"></i>';
+//   li.appendChild(link);
+
+//   // append li to ul
+//   taskList.appendChild(li);
+
+//   // clear input
+//   taskInput.value = '';
+
+//   e.preventDefault();
+// }
+
+/*
+This function ```removeTask``` is going to take an event parameter ```e``` and then remember, we need to target the ```x``` icon. So, we need to check and create a condition for that particular target. Use ```e.target.parentElement.classList.contains``` to get the ```<a>``` link.
+
+Remember, we want the whole ```<li>``` to remove when the target element is clicked on. Right now the setup is, when you click on the target element, it logs ```<i>...</i>``` icon level, but we want to remove up to two levels of parent element, the ```<a>``` and then ```<li>```.
+
+We can also add confirmation to check if the user really wants to remove the task.
+*/
+
+// FUNCTION TO REMOVE TASK
+
+// function removeTask(e) {
+//   if (e.target.parentElement.classList.contains('delete-item')) {
+//     // console.log(e.target);   // Test, logs <i>..</i>
+//     if (confirm('Are you sure you want to remove task?')) {
+//       e.target.parentElement.parentElement.remove();
+//     }
+//   }
+// }
+
+
+// ======================================
+// CLEAR Task Button Function
+// ======================================
+/*
+Create another event listener for the button to clear task list ```clearBtn.addEventListner('click', clearTasks);``` in the ```init()``` function.
+
+In ```clearTask``` function, there's actually two different ways of approaching this. First, you can do ```taskList.innerHTML = '';``` and another way is to loop through with a ```while(){}``` loop and then remove each one, and the while loop way is actually faster to process (visit, https://jsperf.com/innerhtml-vs-removechild/47)
+
+The while-loop, while there is a first child in the task list, ```taskList.firstChild``` meaning that there's still something in the list, then we want to ```taskList.removeChild(taskList.firstChild)```
+*/
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// INITIALIZE
+init();
+
+// FUNCTION TO LOAD EVENT LISTENERS
+function init() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearButton.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// FUNCTION TO ADD TASK
 function addTask(e) {
   // check if input field is empty
   if (taskInput.value === '') {
@@ -130,4 +263,56 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// FUNCTION TO REMOVE TASK
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    // console.log(e.target);   // Test, logs <i>..</i>
+    if (confirm('Are you sure you want to remove task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// FUNCTION TO CLEAR TASK
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // while loop is faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// ======================================
+// FILTER Through the Tasks
+// ======================================
+/*
+Add another event listener for filter, ```filter.addEventListener('keyup', filterTasks);``` inside the ```init()``` function, it's going to use the 'keyup' event type and call the filterTasks function.
+
+First, get the value of whatever that's being typed in the input field. Turn it to lowercase so we can match correctly even if the user accidently has the caps-lock on.
+
+Next, take all of the list items with ```document.querySelectorAll('.collection-item').forEach();``` and we can use ```forEach()``` bc ```querySelectorAll returns a ```NodeList```, but if we used ```getElementByClassName``` that would return an ```HTML Collection``` which we would have to convert to an array in order to use ```forEach()```. In the ```forEach()``` pass a function with ```task``` as a parameter for our iterator. Then, set a variable for ```item``` and set it to ```task.firstChild``` and then we want the ```textContent``` of that ```firstChild```. 
+
+Afterwards, create a condition to check inside of ```document.querySelectorAll('.collection-item').forEach(function(task) {...}``` if ```item.toLowerCase()``` to make in not case-sensitive, and ```indexOf(text)``` and pass in the ```text``` that's the text that's being typed, we want to search in our list and pass it to ```indexOf()```, if there's no match, it's going to equal to ```-1``` so we want to say, if it's not equal to negative one ```!=``` then we want the task to show, set it to show with ```task.style.display = 'block';``` else hide it with 
+```task.style.display = 'none';```
+*/
+
+function filterTasks(e) {
+  // get the input field value
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+    // console.log(item);  // test
+  });
+
+  // console.log(text); // test, it'll log input on keyup
 }
