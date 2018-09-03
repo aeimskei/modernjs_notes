b47a7e021be3207c5f1a0723614b8dc0d124ce10
Amy Boudsady <aeimskei@gmail.com>
2018-09-02 17:12:18 -0700

    Dynamically assign value to min and max from JS to UI

--- 02_domapps/02_guessnum/guess.js
+++ 02_domapps/02_guessnum/guess.js
@@ -1,7 +1,7 @@
 // Variables for Game Values
 let min = 1;
-let max = 10;
-let winningNum = 2;
+let max = 20;
+let winningNum = 19;
 let guessesLeft = 3;
 
 // Variables for UI Elements
@@ -11,3 +11,7 @@ const maxNumber = document.querySelector('.max-number');
 const guessButton = document.querySelector('#guess-button');
 const guessInput = document.querySelector('#guess-input');
 const message = document.querySelector('.message');
+
+// Assign UI min and max to show dynamically, so remove hard code html
+minNumber.textContent = min;
+maxNumber.textContent = max;
