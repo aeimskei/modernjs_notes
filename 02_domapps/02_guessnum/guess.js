e4e3a0f8ba33f875155e73bf3a83afdc043b7690
Amy Boudsady <aeimskei@gmail.com>
2018-09-02 18:50:27 -0700

    Setup validation

--- 02_domapps/02_guessnum/guess.js
+++ 02_domapps/02_guessnum/guess.js
@@ -15,3 +15,25 @@ const message = document.querySelector('.message');
 // Assign UI min and max to show dynamically, so remove hard code html
 minNumber.textContent = min;
 maxNumber.textContent = max;
+
+// Create event listener for button to listen for click
+guessButton.addEventListener('click', submitGuess);
+
+// Function action when button is clicked
+function submitGuess() {
+  let guess = parseInt(guessInput.value);
+  // console.log(guess);
+  // console.log(typeof guess);
+
+  // Validate input check to make it's not blank, not less than min or above max and is a number
+  if (isNaN(guess) || guess < min || guess > max) {
+    // if any of the above is true, alert a message
+    setMessage(`Please enter a number between ${min} and ${max}.`, 'red')
+  }
+}
+
+// Function for setMessage
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
