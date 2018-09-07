7cc2cc13231f1597c9be7610def02d1c4ebf237c
Amy Boudsady <aeimskei@gmail.com>
2018-09-07 11:57:33 -0700

    Setup event listener and test if grabbing input values

--- 04_oopapp/00_es5booklist/booklist.js
+++ 04_oopapp/00_es5booklist/booklist.js
@@ -10,10 +10,21 @@ function UI() {}
 
 // Set Variables
 const form = document.querySelector('#book-form');
+let title = document.querySelector('#title'),
+    author = document.querySelector('#author'),
+    isbn = document.querySelector('#isbn'),
+    bookList = document.querySelector('#book-list')
 
 // Assign Event Listeners
-form.addEventListener('submit', function(e) {
-  console.log('test');
+form.addEventListener('submit', submitBook)
+
+// Helper Function for Submit
+function submitBook(e) {
+  // Values we want from Form
+  title = title.value;
+  author = author.value;
+  isbn = isbn.value;
+  console.log(title, author, isbn); // test log
 
   e.preventDefault();
-})
+}
